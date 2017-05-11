5e046c2455e5fdde578d693964276c5e1c122b00
TrueHub <379205391@qq.com>
2017-05-11 23:23:29 +0800

    电池view

--- myveiw/src/main/java/com/ec/vone/MainActivity.java
+++ myveiw/src/main/java/com/ec/vone/MainActivity.java
@@ -2,21 +2,43 @@ package com.ec.vone;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.SeekBar;
 
-import com.ec.vone.view.LinechartView;
-
-import static android.R.attr.value;
+import com.ec.vone.view.BattaryView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BattaryView battaryView ;
+    private SeekBar seekBar ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                battaryView.setBattaryPercent(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
+    private void initView() {
+        battaryView = (BattaryView) findViewById(R.id.battery_view);
+        seekBar = (SeekBar) findViewById(R.id.seek);
+    }
 
 
 }
--- myveiw/src/main/java/com/ec/vone/view/BattaryView.java
+++ myveiw/src/main/java/com/ec/vone/view/BattaryView.java
@@ -2,15 +2,12 @@ package com.ec.vone.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
-import android.support.annotation.Nullable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -23,23 +20,26 @@ import com.ec.vone.R;
 public class BattaryView extends View {
 
     private final Paint mPaint;
-    private int battaryPercent;
-    private int color;
-    private String text;
-    private Bitmap img;
+    private int battaryPercent;//电量百分比
+    private int color;//前景色
+    private float round;//圆角大小，这里写死，省去使用时计算
+
+    public void setBattaryPercent(int battaryPercent) {
+        this.battaryPercent = battaryPercent;
+        postInvalidate();
+    }
 
     public BattaryView(Context context) {
         this(context, null);
     }
 
-    public BattaryView(Context context, @Nullable AttributeSet attrs) {
+    public BattaryView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public BattaryView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public BattaryView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.BattaryView, 0, defStyleAttr);
-
         for (int i = 0; i < typedArray.length(); i++) {
             int attr = typedArray.getIndex(i);
             switch (attr) {
@@ -51,11 +51,9 @@ public class BattaryView extends View {
                     break;
             }
         }
-        text = battaryPercent + "%";
         mPaint = new Paint();
         mPaint.setColor(color);
         mPaint.setAntiAlias(true);
-        img = BitmapFactory.decodeResource(getResources(), R.mipmap.battery);
     }
 
     @Override
@@ -65,17 +63,13 @@ public class BattaryView extends View {
         int specSize = MeasureSpec.getSize(widthMeasureSpec);
 
         int width = 0, height = 0;
-
         switch (specMode) {
             case MeasureSpec.EXACTLY:
                 width = specSize + getPaddingRight() + getPaddingLeft();
                 break;
-
             case MeasureSpec.AT_MOST:
-                width = (int) (mPaint.measureText(text) + getPaddingLeft() + getPaddingRight() + img.getWidth());//先确定mPaint是否已经设置textsize
                 break;
         }
-
         specMode = MeasureSpec.getMode(heightMeasureSpec);
         specSize = MeasureSpec.getSize(heightMeasureSpec);
         switch (specMode) {
@@ -83,33 +77,47 @@ public class BattaryView extends View {
                 height = specSize + getPaddingTop() + getPaddingBottom();
                 break;
             case MeasureSpec.AT_MOST:
-//                height = specSize + getPaddingTop() + getPaddingBottom();
-                height = img.getHeight() + getPaddingTop() + getPaddingBottom();
-//                height = (int) (Math.abs(mPaint.getFontMetrics().bottom - mPaint.getFontMetrics().top) + getPaddingTop() + getPaddingBottom());
                 break;
         }
-
         setMeasuredDimension(width, height);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        mPaint.setStyle(Paint.Style.FILL);
-        int imgH = img.getHeight();
-        int imgW = img.getWidth();
-        RectF rectF = new RectF((float) getPaddingLeft(),
+        round = getHeight() / 40f;//把圆角写死，省去使用时计算
+        mPaint.setStyle(Paint.Style.FILL);//填充模式
+        //电池区域的高，宽
+        int imgH = getHeight() - getPaddingTop() - getPaddingBottom();
+        int imgW = (getWidth() < getHeight() / 2) ? (getWidth() - getPaddingLeft() - getPaddingRight()) : getHeight() / 2;
+        //根据电量百分比得到的矩形,top和bottom先占位，等一下再重新赋值，也可以new RectF()里面空参，四个属性挨个设置
+        RectF rectF = new RectF(
+                (float) getPaddingLeft(),
                 0,
                 (float) getPaddingLeft() + imgW,
                 0);
         float spaceTop = (getHeight() - imgH) / 2 + getPaddingTop();
         rectF.top = spaceTop + (1.0f - battaryPercent * 1.0f / 100) * imgH;
         rectF.bottom = getHeight() - spaceTop;
+        //上面这三行其实有电多余，由于刚开始是想设置at most时的状况的，后来发现太麻烦，急着用就先这样了。
 
-        canvas.drawRect(rectF, mPaint);
-        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
-        canvas.drawBitmap(img, rectF.left, spaceTop, mPaint);
-
+        int layerId = canvas.saveLayer(0, 0, canvas.getWidth(), canvas.getHeight(), null, Canvas.ALL_SAVE_FLAG);
+        int h1 = imgH / 8;//电池凸点的高度
+        int w1 = imgW / 3;//电池凸点的宽度
+        //绘制背景
+        mPaint.setColor(Color.argb(77, 0, 0, 0));
+        canvas.drawRoundRect(rectF.left,h1 + getPaddingTop(),rectF.right,getHeight() - getPaddingBottom(),round,round,mPaint);
+        canvas.drawRect(getPaddingLeft() + w1,getPaddingTop(),rectF.right - w1,h1 + getPaddingTop(),mPaint);
+        //绘制有电部分
+        canvas.save();
+        canvas.clipRect(rectF);
+        mPaint.setColor(color);
+        canvas.drawRoundRect(rectF.left, getPaddingTop() + h1, rectF.right, rectF.bottom, round, round, mPaint);
+        canvas.drawRect(rectF.left + w1, rectF.top, rectF.right - w1, getPaddingTop() + h1 + h1 / 10, mPaint);
+        canvas.restore();
+        canvas.restoreToCount(layerId);
 
     }
+
 }
