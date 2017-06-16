d02a80f92208b16d810949c9f8dfcef26dffec94
truehub <379205391@qq.com>
2017-06-16 17:40:37 +0800

    新增WaveCircleView______圆形波纹扫描

--- myveiw/src/main/java/com/ec/vone/view/WaveCircleView.java
+++ myveiw/src/main/java/com/ec/vone/view/WaveCircleView.java
@@ -14,12 +14,9 @@ import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.AnticipateOvershootInterpolator;
-import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 
@@ -161,17 +158,17 @@ public class WaveCircleView extends View {
 
     /**
      * 设置水波纹效果
-     * @param interpolator 时间插值器
-     *     AccelerateDecelerateInterpolator 在动画开始与结束的地方速率改变比较慢，在中间的时候加速
-     *     AccelerateInterpolator  在动画开始的地方速率改变比较慢，然后开始加速
-     *     AnticipateInterpolator 开始的时候向后然后向前甩
-     *     AnticipateOvershootInterpolator 开始的时候向后然后向前甩一定值后返回最后的值
-     *     BounceInterpolator   动画结束的时候弹起
-     *     CycleInterpolator 动画循环播放特定的次数，速率改变沿着正弦曲线
-     *     DecelerateInterpolator 在动画开始的地方快然后慢
-     *     LinearInterpolator   以常量速率改变
-     *     OvershootInterpolator    向前甩一定值后再回到原来位置
-     *     PathInterpolator 这个是新增的我说原来怎么记得是9个，这个顾名思义就是可以定义路径坐标，然后可以按照路径坐标来
+     * @param interpolator 时间插值器 默认使用的是 2 AccelerateInterpolator
+     *    0 PathInterpolator 可以定义路径坐标，然后可以按照路径坐标来
+     *    1 AccelerateDecelerateInterpolator 在动画开始与结束的地方速率改变比较慢，在中间的时候加速
+     *    2 AccelerateInterpolator  在动画开始的地方速率改变比较慢，然后开始加速
+     *    3 AnticipateInterpolator 开始的时候向后然后向前甩
+     *    4 AnticipateOvershootInterpolator 开始的时候向后然后向前甩一定值后返回最后的值
+     *    5 BounceInterpolator   动画结束的时候弹起
+     *    6 CycleInterpolator 动画循环播放特定的次数，速率改变沿着正弦曲线
+     *    7 DecelerateInterpolator 在动画开始的地方快然后慢
+     *    8 LinearInterpolator   以常量速率改变
+     *    9 OvershootInterpolator    向前甩一定值后再回到原来位置
      */
     public void setInterpolator(Interpolator interpolator) {
         mInterpolator = interpolator;
